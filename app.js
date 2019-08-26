9780e8605f4b08960c0ed8e1d4c39bf130191931
Michael Rubenstein <mrubenstein6192@gmail.com>
2019-08-26 15:38:40 -0400

    buttons

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
   window.scrollTo(0,0);
   $("#prompt").hide()
+  $("#buttonChoices").hide();
+  $("#practiceSides").hide();
+  $("#practiceProblems").hide();
 
   $(document).on("click", ".start", function() {
     start();
@@ -10,8 +13,25 @@ $(document).ready(function() {
     console.log("Start");
     $("#welcome").hide();
     $("#prompt").show();
+    $("#buttonChoices").show();
 
 
+    $("#basics").on("click", function() {
+      $("#buttonChoices").hide();
+      console.log("Basics");
+      $("#practiceSides").show();
+      $("#practiceProblems").show();
+      $("#seeBasics").hide();
+    })
+
+    $("#skipToProblems").on("click", function() {
+      $("#buttonChoices").hide();
+      console.log("Skip");
+      $("#practiceProblems").show();
+      $("#seeBasics").on("click", function() {
+        $("#practiceSides").show();
+      })
+    })
   }
   
 })
