5b6e6ade4ed19f2764c814e55a6f52d5d506fd2b
Michael Rubenstein <mrubenstein6192@gmail.com>
2019-09-02 11:53:07 -0400

    paths

--- app.js
+++ app.js
@@ -8,7 +8,108 @@ $(document).ready(function() {
   $("#fractionInput").hide();
   $(".nextProblem2").hide();
   // $("#showFirstProblemAgain").hide();
+  function showQuestion() {
+    $(".newProblem").hide();
+    $(".showinfo").hide();
+    $(".nextProblem").hide();
+    // $("#firstButton").hide();
+    var randIndex = Math.floor(Math.random() * 15) + 1
+    console.log(randIndex);
+    console.log(problems[randIndex].question)
+    console.log(problems[randIndex].answer)
+    
+    $(".first").val("");
+    $(".second").val("");
+    var problemImage = new Image(150,150);
+    problemImage.src = problems[randIndex].pic
+    $("#problemPrint").html(problemImage);
+    $("#fractionInput").show();
+    $("#showProb").html(problems[randIndex].question)
+    $(".first").focus();
+    $("#response").hide();
 
+    $form2 = $("<form autocomplete = 'off'></form>");
+    $form2.addClass("guessForm2 ml-0 mt-2 text-right");
+    $form2.append("<div id = optional>(Optional)</div><br>")
+    $form2.append("<div id = oppCheck>Opposite: <span class = 'fup my-1'><input class = 'opp' type = 'text' maxlength = '2' size = '2'></input></span></div>");
+    $form2.append("<div id = adjCheck>Adjacent: <span class = 'fup my-1'><input class = 'adj' type = 'text' maxlength = '2' size = '2'></input></span></div>");
+    $form2.append("<div id = hypCheck>Hypotenuse: <span class = 'fup my-1'><input class = 'hyp' type = 'text' maxlength = '2' size = '2'></input></span></div>");
+    $("#showSides").html($form2)
+
+    $form = $("<form autocomplete = 'off'></form>");
+    $form.addClass("guessForm1 fraction ml-0 mt-2");
+    $form.append("<span class = 'fup'><input class = 'first' type = 'text' maxlength = '2' size = '2'></input></span>");
+    $form.append("<span class = 'bar my-3'></span>")
+    // $form2 = $("<form autocomplete = 'off'></form>");
+    // $form2.addClass("guessForm2 fraction ml-0 mt-2");
+    $form.append("<span class = 'fdn'><input class = 'second' type = 'text' maxlength = '2' size = '2'></input></span>");
+    $("#fractionAppear").html($form)
+    
+    $button = $("<button type = 'submit'>Submit</button>")
+    $button.addClass("submitGuess")
+    $("#buttonAppear").html($button);
+    
+
+    $(".submitGuess").on("click", function() {
+      let opp = $(".opp").val();
+      console.log("You said opp is: " + opp);
+      let adj = $(".adj").val();
+      console.log("You said adj is: " + adj);
+      let hyp = $(".hyp").val();
+      console.log("You said hyp is: " + hyp);
+      console.log("This submitted");
+
+      let numerator = $(".first").val();
+      console.log("You said: " + numerator);
+      let denominator = $(".second").val();
+      console.log("You said: " + denominator);
+      let quotient = numerator / denominator;
+      console.log("Which gives: " + quotient);
+      console.log("The answer is: " + problems[randIndex].answer);
+
+      if (opp == problems[randIndex].opposite) {
+        $("#oppCheck").css("color", "green");
+      }
+      else{
+        $("#oppCheck").css("color","red");
+        $(".opp").val("")
+      }
+      if (adj == problems[randIndex].adjacent) {
+        $("#adjCheck").css("color", "green");
+      }
+      else{
+        $("#adjCheck").css("color","red");
+        $(".adj").val("")
+      }
+      if (hyp == problems[randIndex].hypotenuse) {
+        $("#hypCheck").css("color", "green");
+      }
+      else{
+        $("#hypCheck").css("color","red");
+        $(".hyp").val("")
+      }
+      
+      if (quotient === problems[randIndex].answer) {
+      
+        // showQuestion()
+        $("#response").show();
+        $("#response").html("Correct! Click 'New Problem' for Another!");
+        $("#response").css("color", "green").css("text-transform", "uppercase").css("font-weight", "bold");
+        $("#newProblem").show();
+        $(".nextProblem").show();
+        console.log("Numerator: " + numerator + " | Denominator: " + denominator );
+        $("#buttonAppear").html("✔")
+      }
+      else {
+        $("#response").show();
+        $("#response").html("Sorry that is incorrect. Try again!")
+        $("#response").css("color", "red").css("text-transform", "uppercase").css("font-weight", "bold");
+        $(".first").val('');
+        $(".second").val('');
+        $(".first").focus();
+      }
+    })
+  }
   var problems = [
     {
       id: 1,
@@ -181,9 +282,10 @@ $(document).ready(function() {
     })
 
     $("#skipToProblems").on("click", function() {
-      $("#buttonChoices").hide();
+      // $("#buttonChoices").hide();
+      $("#skipToProblems").hide();
       console.log("Skip");
-      $("#practiceProblems").show();
+     showQuestion();
       $("#seeBasics").on("click", function() {
         $("#practiceSides").show();
       })
@@ -206,108 +308,7 @@ $(document).ready(function() {
       
       showQuestion();
 
-      function showQuestion() {
-      $(".newProblem").hide();
-      $(".showinfo").hide();
-      $(".nextProblem").hide();
-      // $("#firstButton").hide();
-      var randIndex = Math.floor(Math.random() * 15) + 1
-      console.log(randIndex);
-      console.log(problems[randIndex].question)
-      console.log(problems[randIndex].answer)
-      
-      $(".first").val("");
-      $(".second").val("");
-      var problemImage = new Image(150,150);
-      problemImage.src = problems[randIndex].pic
-      $("#problemPrint").html(problemImage);
-      $("#fractionInput").show();
-      $("#showProb").html(problems[randIndex].question)
-      $(".first").focus();
-      $("#response").hide();
-
-      $form2 = $("<form autocomplete = 'off'></form>");
-      $form2.addClass("guessForm2 ml-0 mt-2 text-right");
-      $form2.append("<div id = optional>(Optional)</div><br>")
-      $form2.append("<div id = oppCheck>Opposite: <span class = 'fup my-1'><input class = 'opp' type = 'text' maxlength = '2' size = '2'></input></span></div>");
-      $form2.append("<div id = adjCheck>Adjacent: <span class = 'fup my-1'><input class = 'adj' type = 'text' maxlength = '2' size = '2'></input></span></div>");
-      $form2.append("<div id = hypCheck>Hypotenuse: <span class = 'fup my-1'><input class = 'hyp' type = 'text' maxlength = '2' size = '2'></input></span></div>");
-      $("#showSides").html($form2)
-
-      $form = $("<form autocomplete = 'off'></form>");
-      $form.addClass("guessForm1 fraction ml-0 mt-2");
-      $form.append("<span class = 'fup'><input class = 'first' type = 'text' maxlength = '2' size = '2'></input></span>");
-      $form.append("<span class = 'bar my-3'></span>")
-      // $form2 = $("<form autocomplete = 'off'></form>");
-      // $form2.addClass("guessForm2 fraction ml-0 mt-2");
-      $form.append("<span class = 'fdn'><input class = 'second' type = 'text' maxlength = '2' size = '2'></input></span>");
-      $("#fractionAppear").html($form)
-      
-      $button = $("<button type = 'submit'>Submit</button>")
-      $button.addClass("submitGuess")
-      $("#buttonAppear").html($button);
-      
-
-      $(".submitGuess").on("click", function() {
-        let opp = $(".opp").val();
-        console.log("You said opp is: " + opp);
-        let adj = $(".adj").val();
-        console.log("You said adj is: " + adj);
-        let hyp = $(".hyp").val();
-        console.log("You said hyp is: " + hyp);
-        console.log("This submitted");
-
-        let numerator = $(".first").val();
-        console.log("You said: " + numerator);
-        let denominator = $(".second").val();
-        console.log("You said: " + denominator);
-        let quotient = numerator / denominator;
-        console.log("Which gives: " + quotient);
-        console.log("The answer is: " + problems[randIndex].answer);
-
-        if (opp == problems[randIndex].opposite) {
-          $("#oppCheck").css("color", "green");
-        }
-        else{
-          $("#oppCheck").css("color","red");
-          $(".opp").val("")
-        }
-        if (adj == problems[randIndex].adjacent) {
-          $("#adjCheck").css("color", "green");
-        }
-        else{
-          $("#adjCheck").css("color","red");
-          $(".adj").val("")
-        }
-        if (hyp == problems[randIndex].hypotenuse) {
-          $("#hypCheck").css("color", "green");
-        }
-        else{
-          $("#hypCheck").css("color","red");
-          $(".hyp").val("")
-        }
-        
-        if (quotient === problems[randIndex].answer) {
-        
-          // showQuestion()
-          $("#response").show();
-          $("#response").html("Correct! Click 'New Problem' for Another!");
-          $("#response").css("color", "green").css("text-transform", "uppercase").css("font-weight", "bold");
-          $("#newProblem").show();
-          $(".nextProblem").show();
-          console.log("Numerator: " + numerator + " | Denominator: " + denominator );
-          $("#buttonAppear").html("✔")
-        }
-        else {
-          $("#response").show();
-          $("#response").html("Sorry that is incorrect. Try again!")
-          $("#response").css("color", "red").css("text-transform", "uppercase").css("font-weight", "bold");
-          $(".first").val('');
-          $(".second").val('');
-          $(".first").focus();
-        }
-      })
-    }
+     
     })
     
     }
